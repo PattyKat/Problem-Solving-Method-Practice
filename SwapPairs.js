40a29af862bae9a00e9f33e1c5c4edeaa7453793
Patrice Cottman <patrice.cottman@gmail.com>
2021-02-22 23:26:56 -0500

    Pseudocode and IOCE for the swap pairs in a linked list problem

---
+++ SwapPairs.js
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(head) {
+
+};
+
+//each node val, next:
+
+
+//check if the current node had a next value
+  //if not, then return that node’s value
+  //if the node’s value is null, return empty node
+
+//create
+
+//if the node has a next value
+  //swap the current node with the next node
+//move forward in iteration by two
+
+Node = [ val: , next: ]
+
+Const swapNodePairs = (head) => {
+
+If(!node.value){
+ Return undefined
+}
+If(node.next === ‘undefined’){
+Return {val: val, next=null}
+}
+
+ While( node.next){
+   currentNode = { vale, next}
+   nextNode={val: , next}
+
+   var a = currentNode.val
+  var b = nextNode.val
+
+currentNode.val = b
+nextNode.val= a
+
+
+
+}
+
+
+
+
+}
