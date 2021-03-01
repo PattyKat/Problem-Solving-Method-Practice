1bfbff133af0308dae357cb8130365ccbcb9b056
Patrice Cottman <patrice.cottman@gmail.com>
2021-03-01 15:31:10 -0500

    Started problem-solving process for new toy problem

---
+++ ThreeNumberSum.js
@@ -0,0 +1,19 @@
+//write a function that takes in a non-empty array of disctinct integers and an integer representing the target sum.  The function should find all triplets in the array that sum up to the target and return a two-dimensional array of all these triplets.
+
+//the numbers in each triplet should be in acsending order and the triplets themselves should be ordered in ascending order w/ respect to the numbers they hold.
+
+//sample Input
+  //array = [12, 3, 1, 2, -6, 5, -8, 6]
+  //targetSum = 0
+//sample output
+  //[[-8, 2, 6], [-8, 3, 5], [-6, 1, 5]]
+
+
+function threeNumberSum(array, targetSum){
+
+}
+
+//I: array of intgerts and target integer
+//O: array of arrays w/ 3 integers
+//C: n/a
+//E:  if empty input or no three numbers add to target, return empty array
